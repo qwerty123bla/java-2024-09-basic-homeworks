6c5548ab54c4541a1ad28dd9fc03b63f945e4381
bla <bychyby@mail.ru>
2024-12-02 19:09:15 +0300

    ДЗ 10 выполнено

--- src/main/java/ru/otus/homework/App.java
+++ src/main/java/ru/otus/homework/App.java
@@ -1,7 +1,18 @@
 package ru.otus.homework;
 
+import ru.otus.homework.hw10.PhoneBook;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Привет");
+        PhoneBook book = new PhoneBook();
+
+        book.add("Дементий", "32432434");
+        book.add("Сергей", "123");
+        book.add("Сергей", "345");
+
+
+        System.out.println("Телефоны, зарегестрированные на имя Сергей: " + book.find("Сергей"));
+
+        System.out.println("Содержит ли телефонная книга номер 345" + (book.containsPhoneNumber("345") ? " - Содержит" : " - Не содержит"));
     }
 }
